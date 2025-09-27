a4b0784e0ca149ce66eae2ea3ba41994663c59c2
cf-neto <carlosfdsn2008@gmail.com>
2025-09-27 12:47:08 -0300

    feat: Adicionando AppDbContext (DB)

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using CallFlow.Models;
+
+namespace CallFlow.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
